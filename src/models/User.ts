7230c893312caa714d1ada1ca6bdb38590e22373
Justin Haddock <jhadddock@protonmail.com>
2024-05-10 09:25:34 -0700

    Add User schema

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+import { User as IUser } from '../interfaces/User';
+// Schema
+const userSchema = new Schema<IUser>({
+  id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String },
+  email: { type: String },
+  avatar: { type: String },
+  userProfile: { type: Object },
+  bio: { type: String, required: true, default: 'Hello!' },
+  joinTime: { type: Date, required: true, default: Date.now() },
+  pinnedConversations: { type: Array },
+});
+
+const User = model<IUser>('User', userSchema);
+export default User;
