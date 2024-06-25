bdcee41e7f8b82dd8996a15ca35f1e039c91b7a7
Justin Haddock <jhadddock@protonmail.com>
2024-06-25 11:56:46 -0700

    Add validation rules to schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,28 @@
 import { Schema, model } from 'mongoose';
 import { User as IUser } from '../interfaces/User';
+import isEmail from 'validator/lib/isEmail';
+import { isStrongPassword } from 'validator';
+
+const validateEmail = function (email: string) {
+  return isEmail(email);
+};
+
 // Schema
 const userSchema = new Schema<IUser>({
-  username: { type: String, required: true },
-  password: { type: String },
-  email: { type: String },
+  username: { type: String, required: true, min: 3, max: 36 },
+  password: {
+    type: String,
+    min: 8,
+    max: 256,
+    validate: [
+      isStrongPassword,
+      'Password is not strong enough. Passwords must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    ],
+  },
+  email: {
+    type: String,
+    validate: [validateEmail, 'Invalid email'],
+  },
   avatar: { type: String },
   userProfile: { type: Object },
   bio: { type: String, required: true, default: 'Hello!' },
