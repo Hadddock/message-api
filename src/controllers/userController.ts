dbfc8b25f82e4fa026352b193fe8e317f2ec42d8
Justin Haddock <jhadddock@protonmail.com>
2024-06-25 08:30:50 -0700

    Add signUp controller to replace createUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,17 +1,35 @@
 import User from '../models/User';
 import { RequestHandler } from 'express';
 import passport from 'passport';
+import { isStrongPassword } from 'validator';
 
 import { User as IUser } from './../interfaces/User';
 
-export const createUser: RequestHandler = async (req, res, next) => {
-  const newUser = new User(req.body);
-  await newUser.save();
-  res.json({ message: 'created' });
-};
-
 export const getUser: RequestHandler = async (req, res, next) => {
   res.send('placeholder');
+};
+
+export const signUp: RequestHandler = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const existingUser = await User.findOne({ username: username });
+
+  if (existingUser) {
+    return res.status(400).json({
+      message: 'Username already taken',
+    });
+  }
+
+  if (!isStrongPassword(password)) {
+    return res.status(400).json({
+      message:
+        'Password is not strong enough. Passwords must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    });
+  }
+
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.status(201).json({ message: 'User created' });
 };
 
 export const login: RequestHandler = async (req, res, next) => {
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,20 +1,19 @@
 import express from 'express';
 import {
-  createUser,
   getUser,
   home,
   login,
   logout,
+  signUp,
 } from '../controllers/userController';
 import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.post('/user', asyncHandler(createUser));
-
 router.get('/user', asyncHandler(getUser));
 
 router.post('/login', asyncHandler(login));
 router.get('/logout', asyncHandler(logout));
+router.post('/signup', asyncHandler(signUp));
 router.get('/home', asyncHandler(home));
 export default router;
