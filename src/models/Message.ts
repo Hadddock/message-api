e42c53638df29026bd20bef933c21b312927c395
Justin Haddock <jhadddock@protonmail.com>
2024-06-27 09:34:06 -0700

    Add validation to message model

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -2,6 +2,14 @@ import { Schema, model } from 'mongoose';
 import { Message as IMessage } from '../interfaces/Message';
 // Schema
 const messageSchema = new Schema<IMessage>({
+  content: { type: String, min: 1, max: 1024 },
+  imageUrl: { type: String, min: 10, max: 2048 },
+  conversation: {
+    type: Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true,
+  },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   postTime: { type: Date, required: true, default: Date.now() },
   editTime: { type: Date, required: true },
 });
